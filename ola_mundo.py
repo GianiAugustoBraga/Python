cc3d9beb46241429bfa7c282a7b422cc8ed9aeff
GianiAugustoBraga <115471128+GianiAugustoBraga@users.noreply.github.com>
2023-07-28 08:09:29 -0300

    Add files via upload
    Initial commit

---
+++ ola_mundo.py
@@ -0,0 +1,18 @@
+
+print("Olá Mundo!")
+
+mensagem = "Isso é uma string"
+print(mensagem)
+
+nome = "giani augusto braga"
+print(nome)
+print(nome.title())
+print(nome.upper())
+print(nome.lower())
+
+nome = "Giani"
+sobrenome = "Braga"
+nome_completo = nome + " " + sobrenome
+print(nome_completo)
+
+print('Albert Einsten certa vez disse: "Uma pessoa que nunca cometeu um erro jamais tentou nada novo."')
