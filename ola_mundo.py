cf554639a71e6cb47a5c80ea6e71dce41ab4fddc
GianiAugustoBraga <115471128+GianiAugustoBraga@users.noreply.github.com>
2023-07-31 08:37:20 -0300

    Update ola_mundo.py

--- ola_mundo.py
+++ ola_mundo.py
@@ -16,3 +16,47 @@ nome_completo = nome + " " + sobrenome
 print(nome_completo)
 
 print('Albert Einsten certa vez disse: "Uma pessoa que nunca cometeu um erro jamais tentou nada novo."')
+
+pessoa_famosa = "Albert Einsten: "
+mensagem = '"Uma pessoa que nunca cometeu um erro jamais tentou nada novo."'
+print(pessoa_famosa + mensagem)
+
+soma =  2 + 3
+print(soma)
+
+subtracao = 5 - 2
+print(subtracao)
+
+multiplicacao = 2 * 3
+print(multiplicacao)
+
+divisao = 9/3
+print(divisao)
+
+
+soma =  2.5 + 3.7
+print(soma)
+
+subtracao = 5.5 - 2.1
+print(subtracao)
+
+multiplicacao = 2.3 * 3
+print(multiplicacao)
+
+divisao = 9/3.2
+print(divisao)
+
+
+
+soma =  4 + 4
+print(soma)
+
+subtracao = 9 - 1
+print(subtracao)
+
+multiplicacao = 2 * 4
+print(multiplicacao)
+
+divisao = 16/2
+print(divisao)
+
