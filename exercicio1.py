db74fcbbf612c2bcc14b5e1ba170837046546695
GianiAugustoBraga <115471128+GianiAugustoBraga@users.noreply.github.com>
2023-07-31 11:21:21 -0300

    Add files via upload
    Exercícios 1 e 2

---
+++ exercicio1.py
@@ -0,0 +1,8 @@
+# Calcular a idade de uma pessoa 
+# Criar duas variáveis uma para a data de nascimento e uma para o ano atual
+# Exibir no final uma mensagem com a idade
+
+ano_nascimento = 1999
+ano_atual = 2023
+idade = ano_atual - ano_nascimento
+print("Sua idade é: ",idade)
---
+++ exercicio2.py
@@ -0,0 +1,10 @@
+#Adicione um número inteiro a variável e mostre na tela o seu dobro
+numero = 5
+dobro = numero * 2
+print(dobro)
+
+
+#Solicitando um valor para o usuário digitar
+numero = input("digite um número:")
+dobro = int(numero) * 2
+print(dobro)
